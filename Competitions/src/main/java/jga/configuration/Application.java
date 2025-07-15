d4dd9ecaf0113e42ca9de76b59e5ab2d6cfcbf60
javigoas <52761967+javigoas@users.noreply.github.com>
2025-07-15 10:45:52 +0200

    Add files via upload

---
+++ Competitions/src/main/java/jga/configuration/Application.java
@@ -0,0 +1,13 @@
+package jga.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"jga.*"})
+public class Application {
+    public static void main(String [] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
